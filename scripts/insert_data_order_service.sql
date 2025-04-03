642f3886a6e6360364a1e1a01a35ebe83f01389d
tungdz <dttung2113@gmail.com>
2025-04-03 15:19:05 +0700

    add new insert data file

---
+++ scripts/insert_data_order_service.sql
@@ -0,0 +1,7 @@
+INSERT INTO service_orderstate (name) VALUES
+                                          ('Pending'),
+                                          ('Approved'),
+                                          ('Shipping'),
+                                          ('Cancelled'),
+                                          ('Disapproved'),
+                                          ('Done');
---
+++ scripts/insert_data_payment_service.sql
@@ -0,0 +1,8 @@
+INSERT INTOINSERT INTO services_paymentstate (name) VALUES
+                                            ('Pending'),
+                                            ('Paid'),
+                                            ('Cancelled'),
+                                            ('Fail');
+INSERT INTO services_paymentmethod (name) VALUES
+                                              ('Cash'),
+                                              ('Online');
